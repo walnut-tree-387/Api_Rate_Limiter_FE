f6ae1af1b7adc1298d575be460325b600dfc5272
ataurAGI <ataur.rahaman@anwargroup.com>
2025-06-01 00:10:01 +0600

    config updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 import { resolve } from "path"
 export default defineNuxtConfig({
+  ssr: true,
+  nitro: {
+    preset: 'netlify',
+  },
+  // Optional: ensure SSG is used
+  routeRules: {
+    '/**': { static: true },
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        <h1>Api Rate Limiter</h1>
+    <div class="p-100">
+        <div class="w-full">
+            <label :for="id" class="block mb-1 text-sm font-medium text-gray-700">
+                Please enter your name
+            </label>
+            <input :id="id" v-model="modelValue" :type="type" :placeholder="placeholder"
+                class="w-full px-3 py-2 text-sm border rounded-md shadow-sm outline-none border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition" />
+        </div>
     </div>
 </template>
